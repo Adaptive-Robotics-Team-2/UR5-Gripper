101ccef47cd3d26ab707c09755d8bc8fc18af887
FontysInti <488628@student.fontys.nl>
2024-09-06 09:27:25 +0200

    Added interrupt to main code
    Interrupt now triggers every second

--- GripperCode/src/main.cpp
+++ GripperCode/src/main.cpp
@@ -1,5 +1,22 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include "Adafruit_ZeroTimer.h"
+
+#pragma region TimerVariables
+
+// Timer tester
+Adafruit_ZeroTimer zt3 = Adafruit_ZeroTimer(3);
+
+unsigned long lastInterruptTime = 0; // Store the last interrupt time
+
+#define INTERVAL 1000 // Interval in milliseconds
+
+// Convert interval to seconds
+float intervalSec = INTERVAL / 1000.0;
+
+// Calculate the compare value using the formula
+unsigned long compareValue = 46874 * intervalSec;
+#pragma endregion
 
 enum GripperState
 {
@@ -25,6 +42,30 @@ GripperState currentState = BOOT_UP; // Initial state of the gripper
 
 int closingPosition = 45; // Position to close the gripper
 int openingPosition = 0;  // Position to open the gripper
+
+// Define the interrupt handler
+void TC3_Handler()
+{
+    Adafruit_ZeroTimer::timerHandler(3);
+}
+
+// The timer 3 callback
+void Timer3Callback0()
+{
+    // Get the current time in milliseconds
+    unsigned long currentTime = millis();
+
+    // Calculate the time difference since the last interrupt
+    unsigned long timeDifference = currentTime - lastInterruptTime;
+
+    // Print the time difference in milliseconds
+    Serial.print("Time between interrupts: ");
+    Serial.print(timeDifference);
+    Serial.println(" ms");
+
+    // Update the last interrupt time to the current time
+    lastInterruptTime = currentTime;
+}
 
 void InitGripper()
 {
@@ -86,6 +127,23 @@ void setup()
     pinMode(TRIGGER_PIN, INPUT); // Set the trigger pin as input
     Serial.begin(115200);
     InitGripper();
+
+    /********************* Timer #3, 16-bit, INTERVAL period */
+    zt3.configure(TC_CLOCK_PRESCALER_DIV1024,    // prescaler
+                  TC_COUNTER_SIZE_16BIT,         // bit width of timer/counter
+                  TC_WAVE_GENERATION_MATCH_PWM); // match style
+
+    // Set compare value
+    zt3.setCompare(0, compareValue);
+
+    // Set the callback to be triggered at the defined interval
+    zt3.setCallback(true, TC_CALLBACK_CC_CHANNEL0, Timer3Callback0);
+
+    // Enable the timer
+    zt3.enable(true);
+
+    // Initialize lastInterruptTime
+    lastInterruptTime = millis();
 }
 
 void loop()
